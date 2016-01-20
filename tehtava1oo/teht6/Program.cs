c14fd844f8c13801137e408296a544bfd34e6b3d
ollio <olli.ouninkorpi@gmail.com>
2016-01-20 10:33:02 +0200

    It's time to commit.

---
+++ tehtava1oo/teht6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double matka;
+            Console.Write("Tervetuloa ohjelmamme kayttajaksi. Ole hyva ja syota autolla ajamasi matka kilometrin tarkkuudella:");
+            matka = double.Parse(Console.ReadLine());
+            double kulutus = matka * 0.0702;
+            double hinta = matka * 1.595;
+            Console.Write("Ajamaasi matkaan kului bensaa {0} litraa ja kyseinen maara bensaa maksoi {1} euroa.", kulutus, hinta);
+            Console.ReadLine();
+        }
+    }
+}
