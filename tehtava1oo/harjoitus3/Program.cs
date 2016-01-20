5f13ecb11b725a2657b25f1c11bbe47a4114df87
ollio <olli.ouninkorpi@gmail.com>
2016-01-20 10:18:35 +0200

    More and more

---
+++ tehtava1oo/harjoitus3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harjoitus3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float luku1, luku2, luku3;
+            Console.Write("Tervetuloa taman laskuohjelman pariin. Ole hyva ja syota yksi kokonaisluku:");
+            luku1 = float.Parse(Console.ReadLine());
+            Console.Write("Luku vastaanotettu. Syota nyt toinen luku:");
+            luku2 = float.Parse(Console.ReadLine());
+            Console.Write("Toinen luku vastaanotettu. Syota viela kolmas luku:");
+            luku3 = float.Parse(Console.ReadLine());
+            Console.Write("Syottamiesi lukujen summa on ", luku1 + luku2 + luku3, "ja keskiarvo  ", Average(luku1, luku2, luku3);
+        }
+    }
+}
