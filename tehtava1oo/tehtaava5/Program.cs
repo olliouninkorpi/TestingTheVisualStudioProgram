2d15957a1a28e2bb3a42b0220bbb7a86306666fb
ollio <olli.ouninkorpi@gmail.com>
2016-01-20 10:18:04 +0200

    Sekunttei

---
+++ tehtava1oo/tehtaava5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtaava5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunnit;
+            Console.Write("Syota sekunnit: ");
+            sekunnit = int.Parse(Console.ReadLine());
+            int tunti = sekunnit / 3600;
+            int minuutti = sekunnit / 60 - tunti * 60;
+            int sekunti = sekunnit % 60;
+
+            Console.WriteLine("Aika: {0}h, {1}min {2}s", tunti, minuutti, sekunti);
+            Console.ReadLine();
+        }
+    }
+}
