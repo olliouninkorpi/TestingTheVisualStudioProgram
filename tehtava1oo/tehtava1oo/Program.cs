9b64e91573eb74c74da224d7ee37803289d80aa7
ollio <olli.ouninkorpi@gmail.com>
2016-01-20 09:40:25 +0200

    first commit.

---
+++ tehtava1oo/tehtava1oo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava1oo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ika;
+            Console.Write("Tervetuloa ohjelmaan, ole hyva ja syota ikasi: ");
+            ika = int.Parse(Console.ReadLine());
+            // string line = Console.ReadLine();
+            // int ika = int.Parse(line);
+            if (ika < 18)
+            {
+                Console.WriteLine("Onnittelut, olet alaikainen.");
+            } else if (ika >= 18 && ika <= 65)
+            {
+                Console.WriteLine("Inffo: Olet aikuinen.");
+            } else
+            {
+                Console.WriteLine("Olet seniori-iassa.");
+            }
+            Console.WriteLine("Paina ENTER-nappainta lopettaaksesi.");
+            Console.ReadLine();
+        }
+    }
+}
