0e30cbb85d42e6d209bd0572313189ef33469838
ollio <olli.ouninkorpi@gmail.com>
2016-01-20 10:45:18 +0200

    Another commit

---
+++ tehtava1oo/harj7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harj7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vuosi;
+            Console.Write("Tervetuloa ohjelmaan. Ole hyva ja syota mika tahansa vuosiluku:");
+            vuosi = double.Parse(Console.ReadLine());
+            if ((vuosi % 4 == 0 && vuosi % 100 != 0) || (vuosi % 400 == 0))
+            {
+                Console.WriteLine("Kyseessa on karkausvuosi.");
+            }
+            else {
+                Console.WriteLine("Kyseessa ei ole karkausvuosi.");
+        }
+        Console.ReadLine();
+            
+        }
+    }
+}
